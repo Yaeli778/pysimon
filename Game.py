ef17fa37886f9b825ee00c82d15985af7ab6462c
gimoteco <gimoteco@gmail.com>
2011-12-20 20:47:37 +0000

    Alterei muita coisa

--- Game.py
+++ Game.py
@@ -12,7 +12,43 @@ from Jogo import Simon
 
 pygame.init()
 
+def pisca(lista):
+    for cor in lista :
+        original = cor
+        if cor == "amarelo" :
+            amarelo = pygame.image.load("amarelo.png") 
+            amarelo_rect = amarelo.get_rect()
+            amarelo_rect = amarelo_rect.move((208,208))
+            screen.blit(amarelo, amarelo_rect)
+            pygame.display.flip()
 
+        elif cor == "verde" :
+            verde = pygame.image.load("verde.png")
+            verde = pygame.image.load("verde.png") 
+            verde_rect = verde.get_rect()
+            verde_rect = verde_rect.move((208,10))
+            screen.blit(verde, verde_rect)
+            pygame.display.flip()
+
+        elif cor == "vermelho":
+            vermelho = pygame.image.load("vermelho.png")
+            vermelho = pygame.image.load("vermelho.png") 
+            vermelho_rect = vermelho.get_rect()
+            vermelho_rect = vermelho_rect.move((10,10))
+            screen.blit(vermelho, vermelho_rect)
+            pygame.display.flip()
+
+        else :
+            azul = pygame.image.load("azul.png")
+            azul = pygame.image.load("azul.png") 
+            azul_rect = azul.get_rect()
+            azul_rect = azul_rect.move((10,208))
+            screen.blit(azul, azul_rect)
+            pygame.display.flip()
+
+        time.sleep(1)
+        cor = original
+        pygame.display.flip()
 
 size = width, height = 396, 396
 speed = [1, 0]
@@ -39,29 +75,11 @@ amarelo_rect = amarelo_rect.move(208,208)
 
 jogo = Simon()
 
-def pisca(lista):
-    for cor in lista :
-        original = cor
-        if cor == "amarelo" :
-            amarelo = pygame.image.load("amarelo.png") 
-            amarelo_rect = amarelo.get_rect()
-            screen.blit(amarelo, amarelo_rect)
-        elif cor == "verde" :
-            verde = pygame.image.load("verde.png")
-        elif cor == "vermelho":
-            vermelho = pygame.image.load("vermelho.png")
-        else :
-            azul = pygame.image.load("azul.png")
-        time.sleep(3)
-        cor = original
 while 1:
         for event in pygame.event.get():
             if pygame.key.get_pressed()[pygame.K_RETURN] :
                 if event.type == pygame.MOUSEBUTTONDOWN :
-                    pisca(["amarelo"])
-                    pisca(jogo.vez_do_pc())
-                    
-                                        
+                    pisca(jogo.vez_do_pc())         
                     if vermelho_rect.collidepoint(event.pos):
                         clicado = "vermelho"
                     elif azul_rect.collidepoint(event.pos):
@@ -73,7 +91,7 @@ while 1:
                         
                     print clicado
                     
-                if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.QUIT: sys.exit()
                 
         screen.fill((255,255,255))
         screen.blit(vermelho, vermelho_rect)
@@ -81,7 +99,3 @@ while 1:
         screen.blit(azul, azul_rect)
         screen.blit(amarelo, amarelo_rect)
         pygame.display.flip()
-        
-
-
-
